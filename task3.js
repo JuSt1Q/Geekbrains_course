39ecc19962aa9635036273c0f2f49ce1a77bc154
JuSt1Q <cnhfyybr28@gmail.com>
2018-06-17 15:33:59 +0800

    added insert check

--- task3.js
+++ task3.js
@@ -1,6 +1,11 @@
 var a = process.argv[2];
 var b = process.argv[3];
 
+if ( a < 1 || b > 10000) {
+	console.log('Ваши вводные данные не удовлетворяют условиям: a >= 1 , b <= 10000');
+	process.exit();
+}
+
 function gcd(a, b) {
 	if (b == 0)
 		return Math.abs(a);
